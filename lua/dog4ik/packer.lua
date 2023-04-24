bb2b48972d72116818498b79ca45d0a8773cb591
Vadim <angryvadik@gmail.com>
2023-04-24 15:06:52 +0200

    markdown preview

--- lua/dog4ik/packer.lua
+++ lua/dog4ik/packer.lua
@@ -59,4 +59,5 @@ use {
 use {
   'lewis6991/gitsigns.nvim',
 }
+use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 end)
