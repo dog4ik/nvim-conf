476a8ee759c4f60e17f60525d6947eaa3267310f
Vadim <angryvadik@gmail.com>
2023-04-25 12:07:00 +0200

    add prisma

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all"
-	ensure_installed = {"help","javascript","typescript", "lua", "rust","tsx","json","css","html","astro" },
+	ensure_installed = {"help","javascript","typescript", "lua", "rust","tsx","json","css","html","astro","prisma"},
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
