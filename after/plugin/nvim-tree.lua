a2879fd8e757e7c54ac50f16e841f7a0ce34874d
Vadim <angryvadik@gmail.com>
2023-05-13 08:03:21 +0200

    disable autostart

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -5,22 +5,7 @@ require("nvim-tree").setup({
 	filters = {
 		custom = { "^\\.git$", "node_modules" },
 	},
+	hijack_netrw = false,
 })
 
-local function open_nvim_tree(data)
-	-- buffer is a directory
-	local directory = vim.fn.isdirectory(data.file) == 1
-
-	if not directory then
-		return
-	end
-
-	-- change to the directory
-	vim.cmd.cd(data.file)
-
-	-- open the tree
-	require("nvim-tree.api").tree.open()
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 vim.keymap.set("n", "<leader>tt", api.tree.toggle)
